b574f957175c773035105ac9b128b9434f5be4c1
tranhoaiduong136 <60745952+tranhoaiduong136@users.noreply.github.com>
2022-05-09 11:59:35 +0700

    Update task.c

--- Scheduler/Core/Src/task.c
+++ Scheduler/Core/Src/task.c
@@ -6,3 +6,26 @@
  */
 
 
+#include<main.h>
+#include<task.h>
+void initLED(void){ // Off
+	HAL_GPIO_WritePin(LED_PIN,LED1,1);
+	HAL_GPIO_WritePin(LED_PIN,LED2,1);
+	HAL_GPIO_WritePin(LED_PIN,LED3,1);
+	HAL_GPIO_WritePin(LED_PIN,LED4,1);
+}
+void task1(void){
+	HAL_GPIO_TogglePin(LED_PIN,LED1);
+}
+
+void task2(void){
+	HAL_GPIO_TogglePin(LED_PIN,LED2);
+}
+
+void task3(void){
+	HAL_GPIO_TogglePin(LED_PIN,LED3);
+}
+
+void task4(void){
+	HAL_GPIO_TogglePin(LED_PIN, LED4);
+}
